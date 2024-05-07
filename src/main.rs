5023336f8b623774b81e5fe83d411d8965ef9101
Yannis Storrer <yannis.storrer@web.de>
2024-05-07 14:25:29 +0200

    Implement future parser structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use clap::{arg, command, value_parser};
+use std::{fs, path::PathBuf};
+
+use clap::{arg, command, value_parser, ArgMatches};
 mod parser;
-use parser::SupportedFormats;
+use parser::{ExpressionParser, SupportedFormats};
 
 
 fn main() {
+    // Disable verbose panic for release mode and send error to stderr
+    #[cfg(not(debug_assertions))]
+    use std::panic;
+    #[cfg(not(debug_assertions))]
+    panic::set_hook(Box::new(|panic_info| {
+        if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+            eprintln!("{s:?}");
+        } else {
+            eprintln!("An error ocurred.");
+        }
+    }));
+
     let matches = command!()
         .name("nixify")
         .version("0.1.0")
@@ -12,7 +26,8 @@ fn main() {
             arg!(<FILE>)
                 .required(true)
                 .id("file")
-                .help("The file to convert."),
+                .help("The file to convert.")
+                .value_parser(value_parser!(std::path::PathBuf)),
         )
         .arg(arg!(--"format" <FORMAT>) 
                 .short('f')
@@ -29,5 +44,19 @@ fn main() {
                 .id("name")
                 .help("The name of the program in the nix expression."),
         ).get_matches();
+    handle_matches(matches);
+}
 
+fn handle_matches(matches: ArgMatches) {
+    // Build a new ExpressionParser
+    let expression_parser = ExpressionParser::new();
+
+    // Get arguments from clap
+    let filepath: &PathBuf = matches.get_one("file").unwrap();
+    let format: SupportedFormats = *matches.get_one("format").unwrap();
+    
+    // Parse the file
+    let content = fs::read_to_string(filepath).expect("Error reading given file");
+    let parsed = expression_parser.parse(&content, format).expect("Failed parsing the given file");
+    unimplemented!()
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,9 +1,60 @@
+use std::{collections::HashMap, path::Path, rc::Rc};
+
 use clap::ValueEnum;
 
-#[derive(Debug, Copy, Clone, PartialEq, ValueEnum)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, ValueEnum)]
 #[allow(non_camel_case_types)]
 pub enum SupportedFormats {
     yaml,
     toml,
     json
 }
+
+pub trait Parser {
+    fn parse(&self, content: &str) -> Option<Vec<NixVariable>>;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct NixVariable {
+    pub name: String,
+    pub value: NixVariableValue
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum NixVariableValue {
+    String(String),
+    Number(f64),
+    Path(Box<Path>),
+    Boolean(bool),
+    Null,
+    List(Vec<NixVariableValue>),
+    AttributeSet(HashMap<String, NixVariableValue>),
+}
+
+pub struct ExpressionParser {
+    parsers: HashMap<SupportedFormats, Box<dyn Parser>>
+}
+
+impl ExpressionParser {
+    pub fn new() -> ExpressionParser {
+        ExpressionParser{ parsers: HashMap::new() }
+    }
+
+    pub fn add_parser(mut self, format: SupportedFormats, parser: Box<dyn Parser>) -> Option<ExpressionParser> {
+        if self.parsers.contains_key(&format) {
+            None
+        } else {
+            self.parsers.insert(format, parser);
+            Some(self)
+        }
+    }
+
+    pub fn parse(&self, content: &str, format: SupportedFormats) -> Option<Vec<NixVariable>> {
+        if !self.parsers.contains_key(&format) {
+            None
+        } else {
+            self.parsers[&format].parse(content)
+        }
+    }
+}
+
